5cb5d26268fc4518ffa87e47050dfd4781d726b6
Fajer Altawel <fajertawel@gmail.com>
2026-02-24 20:30:15 +0300

    add about

--- MLBB/src/App.tsx
+++ MLBB/src/App.tsx
@@ -2,6 +2,9 @@
 
 import Footer from './components/Footer'
 import Navbar from './components/NavBar'
+import About from './sectoins/About'
+import Context from './sectoins/Context'
+import Services from './sectoins/Services'
 
 function App() {
  
@@ -11,7 +14,10 @@ function App() {
        <div className='w-full min-w-full flex flex-col min-h-screen'>
       <Navbar />
       <main className='flex-grow overflow-scroll max-h-[75vh]'>
-        
+       
+        <Services />
+        <Context />
+        <About />
       </main>
       <Footer />
     </div>
--- MLBB/src/sectoins/About.tsx
+++ MLBB/src/sectoins/About.tsx
@@ -1,7 +1,12 @@
+import aboutwallpaper from '../assets/aboutwallpaper.png'
+
 export default function About() {
     return (
-        <p className="text-center text-2xl mt-10">
-            This is the about page
-        </p>
+     <div id="about" className="relative flex flex-col items-center justify-center h-[50vh] bg-gray-100 text-center overflow-hidden">
+        <img src={aboutwallpaper} alt="wallpaper" className="h-12 w-auto inset-0 absolute" />
+        <h1 className="text-4xl font-bold">About Us</h1>
+        <h3 className="text-xl mt-4">Learn more about our company and missions</h3>
+     </div>
+    
     )
 }
