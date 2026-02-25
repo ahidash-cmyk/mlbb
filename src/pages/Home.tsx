fe4cbbc04a5c2bde22020702d9949037a62bc7ba
Fajer Altawel <fajertawel@gmail.com>
2026-02-25 18:14:40 +0300

    apdate

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,22 @@
 import About from "../sectoins/About";
+import Blog from "../sectoins/Blog";
+import Contact from "../sectoins/Contact";
 import Hero from "../sectoins/Hero";
+import Portfolio from "../sectoins/Protfolio";
 import Services from "../sectoins/Services";
+import Testimonials from "../sectoins/Testimonials";
 
 export default function Home() {    
     return (
         <>
-          <Hero />
-        <Services />
-        <Context />
+        <Hero />
         <About />
+        <Services />
+        <Portfolio />
+        <Testimonials />
+        <Blog />
+        <Contact />
+        
         </>
     );
 }
